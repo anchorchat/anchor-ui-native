08ae3689e2a15271901c7a5aaa654eec96304bb1
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-29 15:21:46 +0200

    Load fonts.

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+   fontLoaded: false,
+ }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+      'nunito-italic': require('./assets/fonts/Nunito-Italic.ttf'),
+      'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+      'nunito-semi-bold': require('./assets/fonts/Nunito-SemiBold.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    const { fontLoaded } = this.state;
+
+    if (!fontLoaded) {
+      return null;
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.textContainer}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </Text>
       </View>
     );
   }
@@ -20,4 +44,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textContainer: {
+    fontFamily: 'nunito-regular'
+  }
 });
