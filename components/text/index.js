6740743e9f7d068004e6f917e2625942779761ba
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-30 09:36:19 +0200

    Moved styles to styles.js, added override style.

--- components/text/index.js
+++ components/text/index.js
@@ -1,95 +1,7 @@
 import React from 'react';
-import { Text as NativeText, StyleSheet } from 'react-native';
+import { Text as NativeText } from 'react-native';
 import PropTypes from 'prop-types';
-import { fonts, colors } from '../../config';
-
-const style = StyleSheet.create({
-  body: {
-    ...fonts.regular,
-    color: colors.black,
-    fontSize: 14
-  },
-  'body-light': {
-    ...fonts.regular,
-    color: colors.darkGray,
-    fontSize: 14
-  },
-  'body-placeholder': {
-    ...fonts.regular,
-    color: colors.lightGray,
-    fontSize: 14
-  },
-  'body-accent': {
-    ...fonts.regular,
-    color: colors.red,
-    fontSize: 14
-  },
-  'body-contrast': {
-    ...fonts.regular,
-    color: colors.white,
-    fontSize: 14
-  },
-  button: {
-    ...fonts.semiBold,
-    color: colors.red,
-    fontSize: 14
-  },
-  divider: {
-    ...fonts.bold,
-    color: colors.darkGray,
-    fontSize: 14
-  },
-  heading: {
-    ...fonts.semiBold,
-    color: colors.black,
-    fontSize: 17
-  },
-  'heading-contrast': {
-    ...fonts.semiBold,
-    color: colors.white,
-    fontSize: 17
-  },
-  'heading-placeholder': {
-    ...fonts.semiBold,
-    color: colors.lightGray,
-    fontSize: 17
-  },
-  navigation: {
-    ...fonts.regular,
-    color: colors.red,
-    fontSize: 17
-  },
-  'navigation-emphasized': {
-    ...fonts.bold,
-    color: colors.red,
-    fontSize: 17
-  },
-  'navigation-secondary': {
-    ...fonts.regular,
-    color: colors.darkGray,
-    fontSize: 14
-  },
-  tab: {
-    ...fonts.semiBold,
-    color: colors.gray,
-    fontSize: 10
-  },
-  'tab-active': {
-    ...fonts.semiBold,
-    color: colors.red,
-    fontSize: 10
-  },
-  time: {
-    ...fonts.italic,
-    color: colors.lightGray,
-    fontSize: 10
-  },
-  'time-contrast': {
-    ...fonts.italic,
-    color: colors.white,
-    fontSize: 10
-  }
-});
+import styles from './styles';
 
 const types = [
   'body',
@@ -113,15 +25,17 @@ const types = [
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  type: PropTypes.oneOf(types)
+  type: PropTypes.oneOf(types),
+  style: PropTypes.object
 };
 
 const defaultProps = {
-  type: 'body'
+  type: 'body',
+  style: {}
 };
 
-const Text = ({ children, type }) => (
-  <NativeText style={style[type]}>
+const Text = ({ children, type, style }) => (
+  <NativeText style={[styles[type], style]}>
     {children}
   </NativeText>
 );
