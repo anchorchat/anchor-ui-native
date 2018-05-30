c8f53b23d1001ba3d102285eceaea5ffcc019df7
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-30 09:36:27 +0200

    Added backgroundColor to contrast texts.

--- App.js
+++ App.js
@@ -33,19 +33,19 @@ export default class App extends React.Component {
         <Text type="body-light">Changes you make will automatically reload.</Text>
         <Text type="body-placeholder">Shake your phone to open the developer menu.</Text>
         <Text type="body-accent">Shake your phone to open the developer menu.</Text>
-        <Text type="body-contrast">Shake your phone to open the developer menu.</Text>
+        <Text type="body-contrast" style={{ backgroundColor: colors.orange }}>Shake your phone to open the developer menu.</Text>
         <Text type="button">Shake your phone to open the developer menu.</Text>
         <Text type="divider">Shake your phone to open the developer menu.</Text>
         <Text type="heading">Shake your phone to open the developer menu.</Text>
         <Text type="heading-placeholder">Shake your phone to open the developer menu.</Text>
-        <Text type="heading-contrast">Shake your phone to open the developer menu.</Text>
+        <Text type="heading-contrast" style={{ backgroundColor: colors.orange }}>Shake your phone to open the developer menu.</Text>
         <Text type="navigation">Shake your phone to open the developer menu.</Text>
         <Text type="navigation-emphasized">Shake your phone to open the developer menu.</Text>
         <Text type="navigation-secondary">Shake your phone to open the developer menu.</Text>
         <Text type="tab">Shake your phone to open the developer menu.</Text>
         <Text type="tab-active">Shake your phone to open the developer menu.</Text>
         <Text type="time">Shake your phone to open the developer menu.</Text>
-        <Text type="time-contrast">Shake your phone to open the developer menu.</Text>
+        <Text type="time-contrast" style={{ backgroundColor: colors.orange }}>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -54,7 +54,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F7A79F',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
