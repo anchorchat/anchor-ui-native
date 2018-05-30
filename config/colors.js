117b08c2fbdebb353a1af00c8cda591c8a9399dc
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-30 09:31:30 +0200

    Changed color names.

--- config/colors.js
+++ config/colors.js
@@ -3,8 +3,8 @@ export default {
   orange: '#F26D5F',
   black: '#000000',
   white: '#FFFFFF',
-  gray0: '#787878',
-  gray1: '#8E8E93',
-  gray2: '#B2B2B2',
-  gray3: '#F8F8F8'
+  darkGray: '#787878',
+  gray: '#8E8E93',
+  lightGray: '#B2B2B2',
+  lighterGray: '#F8F8F8'
 };
