74df4f6c0a336529f8e33248ae9c04524ccb55c7
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-30 16:59:16 +0200

    Workin on docs.

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,7 +2,7 @@
 import { AppRegistry } from 'react-native';
 import './css/meyer-reset.css';
 import './css/index.css';
-import App from './app';
+import App from './components/app';
 
 AppRegistry.registerComponent('App', () => App);
 
