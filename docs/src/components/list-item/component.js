9b3047614cc65b8346e3dbbdee122f0a7eeb0e4e
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-04 15:55:30 +0200

    Added Props to docs page.

--- docs/src/components/list-item/component.js
+++ docs/src/components/list-item/component.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import faker from 'faker';
+import find from 'lodash/find';
 import Text from '../../anchor-ui-native/text';
 import ListItem from '../../anchor-ui-native/list-item';
 import Avatar from '../../anchor-ui-native/avatar';
 import style from './style';
+import components from '../../components.json';
+import Props from '../props';
+
+const componentData = find(components, { displayName: 'ListItem' });
 
 const ListItemDoc = () => (
   <section className="page">
     <h1 className="heading-large">ListItem</h1>
+    <span><Text type="body-light">{componentData.description}</Text></span>
     <h2><Text type="heading">Examples</Text></h2>
     <section style={style.list}>
       <ListItem
@@ -30,6 +36,12 @@ const ListItemDoc = () => (
         time="14:41"
       />
     </section>
+    <Props props={componentData.props} />
+    <span>
+      <Text type="body-light">
+        Other properties are applied to the root element. See <a href="https://facebook.github.io/react-native/docs/touchablehighlight.html#props" target="blank" rel="noopener noreferrer">https://facebook.github.io/react-native/docs/touchablehighlight.html#props</a> for all available props.
+      </Text>
+    </span>
   </section>
 );
 
