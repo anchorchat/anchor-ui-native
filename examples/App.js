23bd8a11bdbfcbccfb9ac3fa1b0844c7778cbc71
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-05 15:34:07 +0200

    WIP Header component.

--- examples/App.js
+++ examples/App.js
@@ -1,6 +1,6 @@
 /* global alert */
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, StatusBar } from 'react-native';
 import { Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 import {
@@ -12,7 +12,8 @@ import {
   ListItem,
   Button,
   TextInput,
-  MessageInput
+  MessageInput,
+  Header
 } from './anchor-ui-native';
 import { colors, fonts } from './anchor-ui-native/config';
 import Attachment from './icons/attachment';
@@ -58,91 +59,94 @@ export default class App extends React.Component {
 
     return (
       <ThemeProvider>
-        <SafeAreaView style={styles.safeAreaContainer}>
-          <ScrollView contentContainerStyle={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <Text type="body-light">Changes you make will automatically reload.</Text>
-            <Text type="body-placeholder">Shake your phone to open the developer menu.</Text>
-            <Text type="body-accent">Shake your phone to open the developer menu.</Text>
-            <Text type="body-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
-            <Text type="button">Shake your phone to open the developer menu.</Text>
-            <Text type="divider">Shake your phone to open the developer menu.</Text>
-            <Text type="heading">Shake your phone to open the developer menu.</Text>
-            <Text type="heading-placeholder">Shake your phone to open the developer menu.</Text>
-            <Text type="heading-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
-            <Text type="navigation">Shake your phone to open the developer menu.</Text>
-            <Text type="navigation-emphasized">Shake your phone to open the developer menu.</Text>
-            <Text type="navigation-secondary">Shake your phone to open the developer menu.</Text>
-            <Text type="tab">Shake your phone to open the developer menu.</Text>
-            <Text type="tab-active">Shake your phone to open the developer menu.</Text>
-            <Text type="time">Shake your phone to open the developer menu.</Text>
-            <Text type="time-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
-            <Divider />
-            <Divider text="A" />
-            <ContentItem headerText="Mobile" bodyText="+ 31 6 37 40 52 93" divider />
-            <View style={{ flexDirection: 'row' }}>
-              <Avatar source={{ uri: 'https://source.unsplash.com/random/100x100' }} />
-              <Avatar text="BG" />
-              <Avatar text="MO" color="pink" />
-            </View>
-            <ListItem
-              primaryText="Peter Kuiper"
-              divider
+        <StatusBar />
+        <Header
+          primaryText="AnchorUI Native"
+          secondaryText="React Native UI kit for Chat Engines"
+        />
+        <ScrollView contentContainerStyle={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text type="body-light">Changes you make will automatically reload.</Text>
+          <Text type="body-placeholder">Shake your phone to open the developer menu.</Text>
+          <Text type="body-accent">Shake your phone to open the developer menu.</Text>
+          <Text type="body-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
+          <Text type="button">Shake your phone to open the developer menu.</Text>
+          <Text type="divider">Shake your phone to open the developer menu.</Text>
+          <Text type="heading">Shake your phone to open the developer menu.</Text>
+          <Text type="heading-placeholder">Shake your phone to open the developer menu.</Text>
+          <Text type="heading-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
+          <Text type="navigation">Shake your phone to open the developer menu.</Text>
+          <Text type="navigation-emphasized">Shake your phone to open the developer menu.</Text>
+          <Text type="navigation-secondary">Shake your phone to open the developer menu.</Text>
+          <Text type="tab">Shake your phone to open the developer menu.</Text>
+          <Text type="tab-active">Shake your phone to open the developer menu.</Text>
+          <Text type="time">Shake your phone to open the developer menu.</Text>
+          <Text type="time-contrast" style={{ backgroundColor: colors.secondary }}>Shake your phone to open the developer menu.</Text>
+          <Divider />
+          <Divider text="A" />
+          <ContentItem headerText="Mobile" bodyText="+ 31 6 37 40 52 93" divider />
+          <View style={{ flexDirection: 'row' }}>
+            <Avatar source={{ uri: 'https://source.unsplash.com/random/100x100' }} />
+            <Avatar text="BG" />
+            <Avatar text="MO" color="pink" />
+          </View>
+          <ListItem
+            primaryText="Peter Kuiper"
+            divider
+          />
+          <ListItem
+            primaryText="Ian Stewart"
+            secondaryText="'Ie-an'"
+            divider
+          />
+          <ListItem
+            primaryText="Lars Tadema"
+            icon={<Avatar text="LT" color="hotpink" />}
+            divider
+            dividerStyle={{ left: 64 }}
+            rightButton={<Text type="body-accent">I&apos;m a button</Text>}
+          />
+          <ListItem
+            primaryText="Sjaak Luthart"
+            secondaryText="If we connect the monitor, we can get to the HDD monitor through the 1080p SMTP card! If we program the matrix, we can get to the SMTP application through the digital THX system!"
+            onPress={() => alert('herro')}
+            icon={<Avatar text="SL" color="purple" size={64} textStyle={{ fontSize: 32 }} />}
+            divider
+            dividerStyle={{ left: 80 }}
+            time="12:12"
+            secondaryTextProps={{
+              numberOfLines: 2
+            }}
+            iconStyle={{ marginTop: 8, marginBottom: 8 }}
+          />
+          <View style={styles.buttonContainer}>
+            <Button
+              style={styles.button}
+              labelText="Click me!"
+              onPress={() => alert('You pressed me!')}
             />
-            <ListItem
-              primaryText="Ian Stewart"
-              secondaryText="'Ie-an'"
-              divider
+            <Button
+              style={styles.button}
+              labelText="Herro!"
+              icon={<Ionicons name="ios-analytics" size={20} color={colors.primary} />}
+              onPress={() => alert('Button says no!')}
             />
-            <ListItem
-              primaryText="Lars Tadema"
-              icon={<Avatar text="LT" color="hotpink" />}
-              divider
-              dividerStyle={{ left: 64 }}
-              rightButton={<Text type="body-accent">I&apos;m a button</Text>}
-            />
-            <ListItem
-              primaryText="Sjaak Luthart"
-              secondaryText="If we connect the monitor, we can get to the HDD monitor through the 1080p SMTP card! If we program the matrix, we can get to the SMTP application through the digital THX system!"
-              onPress={() => alert('herro')}
-              icon={<Avatar text="SL" color="purple" size={64} textStyle={{ fontSize: 32 }} />}
-              divider
-              dividerStyle={{ left: 80 }}
-              time="12:12"
-              secondaryTextProps={{
-                numberOfLines: 2
-              }}
-              iconStyle={{ marginTop: 8, marginBottom: 8 }}
-            />
-            <View style={styles.buttonContainer}>
-              <Button
-                style={styles.button}
-                labelText="Click me!"
-                onPress={() => alert('You pressed me!')}
-              />
-              <Button
-                style={styles.button}
-                labelText="Herro!"
-                icon={<Ionicons name="ios-analytics" size={20} color={colors.primary} />}
-                onPress={() => alert('Button says no!')}
-              />
-            </View>
-            <TextInput
-              labelText="Text"
-              placeholder="Jot something down..."
-              onChangeText={this.handleTextChange}
-              value={text}
-              divider
-            />
-            <MessageInput
-              placeholder="Message..."
-              onChangeText={this.handleMessageChange}
-              value={message}
-              leftIcon={<Attachment />}
-              rightIcon={message ? <Send color={colors.primary} /> : <Camera />}
-            />
-          </ScrollView>
-        </SafeAreaView>
+          </View>
+          <TextInput
+            labelText="Text"
+            placeholder="Jot something down..."
+            onChangeText={this.handleTextChange}
+            value={text}
+            divider
+          />
+          <MessageInput
+            placeholder="Message..."
+            onChangeText={this.handleMessageChange}
+            value={message}
+            leftIcon={<Attachment />}
+            rightIcon={message ? <Send color={colors.primary} /> : <Camera />}
+          />
+        </ScrollView>
       </ThemeProvider>
     );
   }
