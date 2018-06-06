53166c2e2bdca292aa49b97d2d948335008626d0
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-06 13:42:18 +0200

    Improved example.

--- examples/pages/message.js
+++ examples/pages/message.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, ScrollView, KeyboardAvoidingView, ImageBackground } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, KeyboardAvoidingView, ImageBackground, FlatList, TouchableOpacity } from 'react-native';
+import uuid from 'uuid';
+import format from 'date-fns/format';
+import subMinutes from 'date-fns/sub_minutes';
 import { MessageInput, withSafeArea, Message } from '../anchor-ui-native';
 import Attachment from '../icons/attachment';
 import Send from '../icons/send';
@@ -44,22 +46,71 @@ const getStyles = (safeArea) => (
   })
 );
 
+const INITIAL_STATE = [
+  {
+    key: uuid.v4(),
+    type: 'text',
+    time: subMinutes(new Date(), '5'),
+    body: 'It could also be lots of other people. It also could be a wordsmith sitting on their bed that weights 400 pounds.',
+    align: 'right'
+  },
+  {
+    key: uuid.v4(),
+    type: 'text',
+    time: subMinutes(new Date(), '15'),
+    body: 'Tremblant is based in Canada and has over 90 runs millions of skiers each year.',
+    align: 'left'
+  }
+];
+
 class MessageHighlightExample extends Component {
   state = {
-    message: ''
+    message: '',
+    messages: INITIAL_STATE
   }
 
   handleMessageChange = (value) => this.setState({ message: value })
 
+  handleMessageSend = () => {
+    const { message, messages } = this.state;
+
+    if (!message) {
+      return false;
+    }
+
+    return this.setState({
+      messages: [
+        {
+          key: uuid.v4(),
+          type: 'text',
+          body: message,
+          time: new Date(),
+          align: 'right'
+        },
+        ...messages
+      ],
+      message: ''
+    })
+  }
+
+  renderMessage = ({ item }) => (
+    <Message
+      type={item.type}
+      bodyText={item.body}
+      align={item.align}
+      time={format(item.time, 'HH:mm')}
+    />
+  )
+
   render() {
-    const { message } = this.state;
+    const { message, messages } = this.state;
     const { safeArea } = this.props;
     const styles = getStyles(safeArea);
 
     return (
       <ImageBackground source={require('../assets/images/background.jpg')} resizeMode="cover" style={{ flex: 1 }}>
         <KeyboardAvoidingView style={styles.wrapper} behavior="padding" keyboardVerticalOffset={45 + safeArea.top}>
-          <ScrollView contentContainerStyle={styles.content}>
+          {/* <ScrollView contentContainerStyle={styles.content}>
             <Message
               type="text"
               bodyText="Tremblant is based in Canada and has over 90 runs millions of skiers each year."
@@ -72,13 +123,19 @@ class MessageHighlightExample extends Component {
               time="12:32"
               statusIcon={<Ionicons style={{ paddingLeft: 2 }} name="ios-checkmark" size={16} color={colors.white} />}
             />
-          </ScrollView>
+          </ScrollView> */}
+          <FlatList
+            data={messages}
+            renderItem={this.renderMessage}
+            contentContainerStyle={{ paddingTop: 4, paddingBottom: 4 }}
+            inverted
+          />
           <MessageInput
             placeholder="Message..."
             onChangeText={this.handleMessageChange}
             value={message}
             leftIcon={<Attachment />}
-            rightIcon={message ? <Send color={colors.primary} /> : <Camera />}
+            rightIcon={message ? <TouchableOpacity onPress={this.handleMessageSend}><Send color={colors.primary} /></TouchableOpacity> : <Camera />}
           />
         </KeyboardAvoidingView>
       </ImageBackground>
