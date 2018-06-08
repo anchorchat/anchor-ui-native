c16633046bd1b1fecf3e0139934efb831b31f8d5
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-08 14:43:23 +0200

    Added markdown.

--- docs/src/components/with-safe-area/component.js
+++ docs/src/components/with-safe-area/component.js
@@ -2,6 +2,16 @@
 import React from 'react';
 import Text from '../../anchor-ui-native/text';
 import style from './style';
+import Markdown from '../markdown';
+
+const withSafeArea = `
+\`\`\`jsx
+import withSafeArea from 'anchor-ui-native/with-safe-area';
+import MyComponent from './my-component';
+
+export default withSafeArea(MyComponent);
+\`\`\`
+`;
 
 const WithSafeAreaDoc = () => (
   <section className="page">
@@ -14,6 +24,7 @@ const WithSafeAreaDoc = () => (
       <span><Text type="body-light" style={style.name}>Component</Text><Text>Component to apply safe area to</Text></span>
       <h2><Text type="heading">Returns</Text></h2>
       <span><Text type="body-light" style={style.name}>SafeAreaComponent</Text><Text>Component with safeArea</Text></span>
+      <Markdown title="Usage" markdown={withSafeArea} />
     </section>
   </section>
 );
