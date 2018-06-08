28d2e97bc7337ce38d40f4476e6af6e2b6915f31
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-08 14:37:21 +0200

    Added Markdown component.

---
+++ docs/src/components/markdown.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import marked from 'marked';
+import prism from 'prismjs';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-jsx';
+import Text from '../anchor-ui-native/text';
+
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  markdown: PropTypes.string.isRequired
+};
+
+marked.setOptions({
+  highlight(code, language) {
+    return prism.highlight(code, prism.languages[language]);
+  }
+});
+
+const style = {
+  container: {
+    display: 'inline-block',
+    marginBottom: '8px'
+  }
+};
+
+const Markdown = ({ markdown, title }) => (
+  <section style={style.container}>
+    <span><Text type="body-light">{title}</Text></span>
+    <div
+      dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+    />
+  </section>
+);
+
+Markdown.propTypes = propTypes;
+
+export default Markdown;
