ff834dbe53a356ec804d89740c9336c5e42455d7
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-08 14:45:44 +0200

    Added markdown.

--- docs/src/components/with-theme/component.js
+++ docs/src/components/with-theme/component.js
@@ -3,6 +3,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Text from '../../anchor-ui-native/text';
 import style from './style';
+import Markdown from '../markdown';
+
+const withTheme = `
+\`\`\`jsx
+import withTheme from 'anchor-ui-native/with-theme';
+import MyComponent from './my-component';
+
+export default withTheme(MyComponent);
+\`\`\`
+`;
 
 const WithThemeDoc = () => (
   <section className="page">
@@ -14,6 +24,7 @@ const WithThemeDoc = () => (
       <span><Text type="body-light" style={style.name}>Component</Text><Text>Component to apply theme to</Text></span>
       <h2><Text type="heading">Returns</Text></h2>
       <span><Text type="body-light" style={style.name}>ThemedComponent</Text><Text>Component with theme</Text></span>
+      <Markdown title="Usage" markdown={withTheme} />
     </section>
   </section>
 );
