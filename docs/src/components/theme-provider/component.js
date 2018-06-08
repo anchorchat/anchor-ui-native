26082dccb2828df90e4341a542db657c1e133384
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-06-08 12:23:12 +0200

    Added keys.

--- docs/src/components/theme-provider/component.js
+++ docs/src/components/theme-provider/component.js
@@ -17,9 +17,9 @@ const ThemeProviderDoc = () => (
     <span><Text type="body-light">{componentData.description}</Text></span>
     <h2><Text type="heading">Defaults</Text></h2>
     <h2><Text type="heading-secondary">Colors</Text></h2>
-    {map(colors, (color, name) => <span><Text type="body-light" style={style.name}>{name}:</Text> <Text>{color}</Text></span>)}
+    {map(colors, (color, name) => <span key={name}><Text type="body-light" style={style.name}>{name}:</Text> <Text>{color}</Text></span>)}
     <h2><Text type="heading-secondary">Fonts</Text></h2>
-    {map(fonts, (font, name) => <span><Text type="body-light" style={style.name}>{name}:</Text> <Text>fontFamily: {'{ '}{font.fontFamily}{' }'}</Text></span>)}
+    {map(fonts, (font, name) => <span key={name}><Text type="body-light" style={style.name}>{name}:</Text> <Text>fontFamily: {'{ '}{font.fontFamily}{' }'}</Text></span>)}
     <span><Text type="body">See the <Link to="/style-guide">Styleguide</Link> for a representation.</Text></span>
     <Props props={componentData.props} />
     <span>
