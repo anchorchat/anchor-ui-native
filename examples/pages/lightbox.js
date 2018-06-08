2370e35b98cc4237c67813c58652b9ffe8120540
Ian Stewart <ian.stewart@anchor.chat>
2018-06-08 11:22:55 +0200

    Added Footer to lightbox.

--- examples/pages/lightbox.js
+++ examples/pages/lightbox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { Lightbox, Text } from '../anchor-ui-native';
 import { colors } from '../anchor-ui-native/config';
 
@@ -10,6 +11,11 @@ const HeaderExample = () => (
       leftButton: <Text type="navigation-emphasized" style={styles.leftHeaderButton}>Done</Text>
     }}
     imageUri="https://img.gadgethacks.com/img/97/67/63657326555041/0/10-awesome-free-dark-themes-for-your-galaxy-s9.w1456.jpg"
+    primaryFooterText="Sjaak Luthart"
+    secondaryFooterText="Today at 09:34"
+    descriptionFooterText="The home screen of a samsung s9"
+    leftFooterButton={<Ionicons name="ios-share-outline" size={32} color={colors.white} style={styles.leftFooterButton} />}
+    rightFooterButton={<Ionicons name="ios-trash-outline" size={32} color={colors.white} style={styles.rightFooterButton} />}
   />
 );
 
@@ -17,6 +23,12 @@ const styles = StyleSheet.create({
   leftHeaderButton: {
     paddingLeft: 17,
     color: colors.white
+  },
+  leftFooterButton: {
+    marginLeft: 11
+  },
+  rightFooterButton: {
+    marginRight: 11
   }
 });
 
