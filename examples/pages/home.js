5e19c48933afdeae9f7f4317ade51bbfa65089f5
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-09-12 16:57:17 +0200

    Reverted changes.

--- examples/pages/home.js
+++ examples/pages/home.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Audio } from 'expo';
-import round from 'lodash/round';
-import { Text, Touchable } from '../anchor-ui-native';
-import IconSend from '../anchor-ui-native/icons/send';
-import IconClose from '../anchor-ui-native/icons/close';
+import { Text } from '../anchor-ui-native';
 import { colors } from '../anchor-ui-native/config';
 
 const navigationOptions = () => ({
@@ -23,71 +19,14 @@ const styles = StyleSheet.create({
   }
 });
 
-class Home extends Component {
-  state = {
-    status: {
-      isPlaying: false,
-      isLoaded: false,
-    },
-    percentage: 0
-  }
-
-  async componentDidMount() {
-    try {
-      this.audio = await Audio.Sound.create(
-        require('../assets/sound.mp3'),
-        { progressUpdateIntervalMillis: 1000 },
-        this.handleUpdate
-      )
-      // await bla.sound.playAsync()
-      // await soundObject.playAsync();
-      // Your sound is playing!
-    } catch (error) {
-      console.log('error', error);
-      // An error occurred!
-    }
-  }
-
-  handleUpdate = (status) => {
-    this.setState({
-      status,
-      percentage: status.positionMillis && status.positionMillis !== 0
-        ? round((status.positionMillis / status.playableDurationMillis) * 100, 2)
-        : 0
-    });
-  }
-
-  handlePlay = async () => {
-    await this.audio.sound.playAsync();
-  }
-
-  handlePause = async () => {
-    await this.audio.sound.pauseAsync();
-  }
-
-  render() {
-    const { status, percentage } = this.state;
-    // console.log(percentage);
-
-    return (
-      <View style={styles.wrapper}>
-        <Text type="heading" style={styles.headingLarge}>AnchorUI Native</Text>
-        {/* <Text>
-          Welcome in the AnchorUI Native examples. View the components by using the drawer navigation.
-        </Text> */}
-        {
-          status.isPlaying
-            ? <Touchable onPress={this.handlePause}><IconClose color="red" /></Touchable>
-            : <Touchable onPress={this.handlePlay}><IconSend color="green" /></Touchable>
-        }
-        <View style={{ width: 200, height: 5, borderRadius: 5, overflow: 'hidden' }}>
-          <View style={{ width: `${percentage}%`, height: 5, backgroundColor: 'hotpink' }} />
-        </View>
-        <Text>{percentage}</Text>
-      </View>
-    );
-  }
-}
+const Home = () => (
+  <View style={styles.wrapper}>
+    <Text type="heading" style={styles.headingLarge}>AnchorUI Native</Text>
+    <Text>
+      Welcome in the AnchorUI Native examples. View the components by using the drawer navigation.
+    </Text>
+  </View>
+);
 
 Home.navigationOptions = navigationOptions;
 
